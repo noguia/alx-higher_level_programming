195c87edd82365a4caf2ac626f89c07d80d8a342
noguia <noguiaisback@gmail.com>
2023-06-06 17:41:13 -0700

    task 13 fixed

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,39 +1,53 @@
 #include "lists.h"
+#include <stdlib.h>
+
+/** by noguia */
+
+/**
+ * add_head - add a node to the head of a linked list
+ * @head: pointer to head node
+ * @node: node to be assigned as head
+ *
+ * Return: pointer to node
+ */
+
+listint_t *add_head(listint_t **head, listint_t *node)
+{
+	node->next = *head;
+	*head = node;
+	return (node);
+}
+
+/**
+ * insert_node - inserts node in linked list
+ * @head: pointer to the head node
+ * @number: number data of the node
+ * Description: insert a new node at the right position in a sorted
+ * linked list
+ *
+ * Return: new node's adress (Success) | NULL (Failure)
+ */
 
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *curr, *tmp;
-    listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *tmp, *new_node;
 
-    if (!new_node)
-        return (NULL);
-    new_node->n = number;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = number, new_node->next = NULL;
 
-    if (!*head)
-    {
-        new_node->next = NULL;
-        *head = new_node;
-        return (new_node);
-    }
-    if ((*head)->n >= number)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-    }
-    curr = *head;
-    while (curr->next)
-    {
-        if (curr->next->n >= number)
-        {
-            tmp = curr->next;
-            curr->next = new_node;
-            new_node->next = tmp;
-            return (new_node);
-        }
-        curr = curr->next;
-    }
-    curr->next = new_node;
-    new_node->next = NULL;
-    return (new_node);
+	if (!(*head) || (*head)->n > number)
+		return (add_head(head, new_node));
+
+	tmp = *head;
+	while (tmp && tmp->next)
+	{
+		if (tmp->next->n > number)
+			break;
+		tmp = tmp->next;
+	}
+	new_node->next = tmp->next;
+	tmp->next = new_node;
+	return (new_node);
 }
