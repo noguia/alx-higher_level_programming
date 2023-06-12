24675202923257bff10babeedf48189be6b4fa9c
noguia <noguiaisback@gmail.com>
2023-06-12 10:52:39 -0700

    task 13 Wr

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,59 +1,22 @@
 #include "lists.h"
+#include <stdlib.h>
 
-/**
- * allocate_memory - allocate required memory for an array
- * @head: head node of listint_t
- *
- * Return: allocated array
- */
-int *allocate_memory(listint_t *head)
-{
-	int *arr;
-	size_t size;
-
-	size = 0;
-	while (head)
-		size++, head = head->next;
-
-	arr = malloc(sizeof(int) * size);
-	if (!arr)
-		exit(1);
-	return (arr);
-}
-
-/**
- * is_palindrome - checks if a list is palindrom
- * @head: pointer to head node of list
- *
- * Return: 1 (Palindrom) | 0 (Not a Palindrom)
- */
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp;
-	int i, *values;
+	listint_t *curr = *head;
+	static listint_t *tmp;
 
-	if (!head || !(*head))
+	if (curr == NULL)
 		return (1);
 
-	values = allocate_memory(*head);
-	tmp = *head, i = -1;
-	while (tmp)
-	{
-		values[++i] = tmp->n;
-		tmp = tmp->next;
-	}
+	if (tmp == NULL)
+		tmp = curr;
 
-	tmp = *head;
-	for (; i >= 0; i--)
+	if (is_palindrome(&curr->next) && tmp->n == curr->n)
 	{
-		if (values[i] != tmp->n)
-		{
-			free(values);
-			return (0);
-		}
 		tmp = tmp->next;
+		return (1);
 	}
-
-	free(values);
-	return (1);
+	else
+		return (0);
 }
