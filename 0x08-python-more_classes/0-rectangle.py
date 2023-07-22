02a4a7cbf319092774e7cc014cbd6fb24c06870d
noguia <noguiaisback@gmail.com>
2023-07-21 19:24:06 -0700

    test up date

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 0-rectangle
+Contains class Rectangle
+Empty class
+Made by My Dad
+"""
+
+
+class Rectangle():
+    """
+    Defines a class empty  rectangle
+    """
+
+    pass
