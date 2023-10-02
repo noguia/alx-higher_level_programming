f8ab67c2af53f7949aead6c532639c135db1a7ce
noguia <noguiaisback@.com>
2023-10-02 01:17:23 +0000

    all good inchallah

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
-"""
-This module contains a function to return peek of a list
-"""
+# made by noguiaa
+'''
+Returns the peak of an unsorted lst
+'''
+ret = int or None
 
 
-def find_peak(ints):
-    """
-    finds a peak in a list of unsorted integers
-    """
-    if not ints or type(ints) is not list:
+def find_peak(list_of_integers) -> ret:
+    '''
+    Finds a numb that is greater than both left & right
+    '''
+
+    listLen = len(list_of_integers)
+    if listLen == 0:
         return None
 
-    left, right = 0, len(ints) - 1
-    while left < right:
-        mid = left + (right - left) // 2
-        condition_1 = (mid - 1 < 0 or ints[mid - 1] <= ints[mid])
-        condition_2 = (mid + 1 >= len(ints) or ints[mid + 1] <= ints[mid])
-        if condition_1 and condition_2:
-            return ints[mid]
-        elif mid - 1 >= 0 and ints[mid - 1] > ints[mid]:
-            right = mid - 1
-        elif mid + 1 < len(ints) and ints[mid + 1] > ints[mid]:
-            left = mid + 1
+    tmp = list_of_integers
+    i = 0
+    n = listLen - 1
 
-    return ints[left]
+    if tmp[i] > tmp[i+1]:
+        return tmp[i]
+    if tmp[n] > tmp[n-1]:
+        return tmp[n]
+
+    x = (i + n) // 2
+    if tmp[x-1] < tmp[x] and tmp[x+1] < tmp[x]:
+        return tmp[x]
+    if tmp[x] < tmp[x-1]:
+        return find_peak(tmp[i:x+1])
+    if tmp[x] < tmp[x+1]:
+        return find_peak(tmp[x:n+1])
+    return tmp[i]
